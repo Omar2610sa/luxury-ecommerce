d2b17528bd081fbace9beaf1c4a30cdadfa85a60
Omar2610sa <118198753+Omar2610sa@users.noreply.github.com>
2026-06-22 14:23:28 +0300

    lss

--- app/CheckOut/page.tsx
+++ app/CheckOut/page.tsx
@@ -1,17 +1,17 @@
 import { BreadCrumb } from "@/components/Breadcrumb/BreadCrumb";
-import { AddressData, CartData } from "@/interfaces/interfaces";
+import { Address, CartData } from "@/interfaces/interfaces";
 import CheckoutClient from "@/sections/CheckoutClient/CheckoutClient";
 
 import { serverApi } from "@/services/serverApi";
 
 export default async function page() {
-    const { data: Address } = await serverApi<{ data: AddressData }>(`address`)
+    const { data: addresses } = await serverApi<{ data: Address[] }>(`address`)
     const { data: cart } = await serverApi<CartData>("cart")
 
     return (
         <div className="container flex flex-col gap-8">
             <BreadCrumb secondLink="تأكيد الطلب" thirdLink="الدفع" />
-            <CheckoutClient cart={cart} Address={Address} />
+            <CheckoutClient cart={cart} Address={addresses} />
         </div>
     )
 }
